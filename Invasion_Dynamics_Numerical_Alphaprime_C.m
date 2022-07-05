7cf41fa305397f3c52bc2a89349012d4b6242f2c
phil-liu1020 <72927467+phil-liu1020@users.noreply.github.com>
2022-07-05 13:51:23 +0100

    Add files via upload

--- Invasion_Dynamics_Numerical_Alphaprime_C.m
+++ Invasion_Dynamics_Numerical_Alphaprime_C.m
@@ -1,11 +1,11 @@
-function alpha0 =Invasion_Dynamics_Numerical_Alphaprime_C(beta,C,alpha0,A,M,tf,fs0,theta1,deltaalpha,m,plots)
+function alpha0 =Invasion_Dynamics_Numerical_Alphaprime_C(beta,C,alpha0,A,M,tf,fmut0,theta1,deltaalpha,m,plots)
 
 % This function iterates the invasion dynamics until fixation of the invading gamete with different encounter rate is reached. In our model, we consider fixation to be reached at the time when the change in frequency of the invader from one generation to the next is less than theta1.
 % List of parameters: deltaalpha - difference between mutant encounter rate and resident encounter rate.
 %                     theta1 - change in frequency between a generation for the system to be deemed for fixation. This is usually a small parameter.
 %                     tf - fertilisation period
 %                     alpha0 - resident gamete encounter rate
-%                     fs0 - initial frequency of mutant
+%                     fmut0 - initial frequency of mutant
 %                     M - mass of adult
 %                     A - number of adults at start of each generation
 %                     beta - resistance to survival of a gamete/agamete
@@ -24,9 +24,9 @@ function alpha0 =Invasion_Dynamics_Numerical_Alphaprime_C(beta,C,alpha0,A,M,tf,f
 %                     exp(-beta/m) is S(beta,m), the survival probability of a body (either a resident or a mutant) of mass m.
 %                     if the change in encounter rate is negative and the initial encounter minus the change in encounter rate is less than 0, then deltaalpha is what it is, otherwise multiply deltaalpha by a randomly generated sign.
 %                     Parameters inside the loop:
-%                                          Na0 and Ns0 are the numbers of resident and mutants in the gamete pool in the beginning of each fertilisation generation.
+%                                          Nres0 and Nmut0 are the numbers of resident and mutants in the gamete pool in the beginning of each fertilisation generation.
 %                                          ws and wa are the fitnesses of the mutant and resident respectively.
-%                                          fs is the frequency of the mutant at the each generation.
+%                                          fmut is the frequency of the mutant at the each generation.
 %                                          deltaalpha is the change in mutant frequency at each generation. 
 
 
@@ -36,25 +36,25 @@ else
 deltaalpha=deltaalpha*sign(randn);                                         
 end
 g=0;                                                             
-deltafs=1;                                                                 
+deltafmut=1;                                                                 
 i=0;
 
-while deltafs>theta1
+while deltafmut>theta1
 
-   Na0=(A*M*(1-fs0))/m;                                                    
-   Ns0=(A*M*fs0)/m;                                                        
+   Nres0=(A*M*(1-fmut0))/m;                                                    
+   Nmut0=(A*M*fmut0)/m;                                                        
 
-   [t,x] = ode45(@(t,x)unisexual_alphaprime_ODE(t,x,deltaalpha,alpha0),[0,tf],[Na0,Ns0,0,0,0]);  
+   [t,x] = ode45(@(t,x)unisexual_alphaprime_ODE(t,x,deltaalpha,alpha0),[0,tf],[Nres0,Nmut0,0,0,0]);  
 
-   ws=(Ns0-x(length(x(:,1)),2))*exp(-beta/(2*m))*(1-C)+x(length(x(:,1)),2)*exp(-beta/m);    
+   ws=(Nmut0-x(length(x(:,1)),2))*exp(-beta/(2*m))*(1-C)+x(length(x(:,1)),2)*exp(-beta/m);    
 
-   wa=(Na0-x(length(x(:,1)),1))*exp(-beta/(2*m))*(1-C)+x(length(x(:,1)),1)*exp(-beta/m);
+   wa=(Nres0-x(length(x(:,1)),1))*exp(-beta/(2*m))*(1-C)+x(length(x(:,1)),1)*exp(-beta/m);
 
-   fs=ws/(ws+wa);
-   deltafs=abs(fs-fs0);                                          
+  mut=ws/(ws+wa);
+   deltafmut=abs(fmut-fmut0);                                          
    
   
-   fs0=fs;
+   fmut0=fmut;
    i = i + 1;
 
    if length(g)>10000
@@ -63,13 +63,13 @@ while deltafs>theta1
    end
 
    if i<=length(g)
-   g(i)=fs0;
+   g(i)=fmut0;
    else
-   g(end+1) = fs0; 
+   g(end+1) = fmut0; 
    end
    
 end
-assignin('base','fs',fs)
+assignin('base','fmut',fmut)
 assignin('base','i',i)
 assignin('base','g',g)
 
@@ -80,7 +80,7 @@ if plots==1
 else
 end
 
-if fs>0.5
+if fmut>0.5
     alpha0 = alpha0+deltaalpha;
 else
 end
